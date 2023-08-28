7d29f7042bbb2dd67adadb9cd95ada8c6204e54f
mustafa-tj <mustafatj7@gmail.com>
2023-08-28 22:36:23 +0530

    Routers and filters

---
+++ Express js/task 3/app.js
@@ -0,0 +1,24 @@
+// Routers And Filters
+
+const express = require('express');
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
+
+
+
+
+app.listen(3000)
