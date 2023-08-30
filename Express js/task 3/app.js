04f18dc298d7141098509babf6fd73d3ab6da664
mustafa-tj <mustafatj7@gmail.com>
2023-08-30 08:19:16 +0530

    Serving HTML,CSS and JS (VIEWS)-Task5

--- Express js/task 3/app.js
+++ Express js/task 3/app.js
@@ -3,22 +3,27 @@
 const express = require('express');
 const app = express();
 
+const path = require('path')
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactusRoutes = require('./routes/contactus');
 
 const bodyParser = require('body-parser');
+const exp = require('constants');
 
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use('/shop',shopRoutes);
+app.use(contactusRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
-})
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
 
 
-
-
-app.listen(3000)
+app.listen(3000);
